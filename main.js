51eb1c2f3f9df36496e8fa32ec1f4abb7fe2b4df
Marcos Oliveira <marcosoliveeira1@users.noreply.github.com>
2024-05-23 01:13:18 -0300

    set new body limit

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 10000;
 const app = express();
 
 // Middleware to parse JSON bodies
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cors({
     origin: '*',
 }));
