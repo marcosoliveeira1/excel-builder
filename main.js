6b999b6c3d70106ef560993f38d1abeee6aa2ec9
Marcos Oliveira <marcosoliveeira1@users.noreply.github.com>
2024-05-23 00:41:17 -0300

    change default port

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 10000;
 
 const app = express();
 
